340a276e9f64bd957c197024f4b1957dabcf1afc
chenchen <929215951@qq.com>
2026-01-16 17:49:55 +0800

    fix: init-study-test-item

---
+++ src/pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+    <button class="btn" @click="goToHome">跳转到首页</button>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('Hello World - Updated')
+
+const goToHome = () => {
+  uni.navigateTo({
+    url: '/pages/home/home'
+  })
+}
+</script>
+
+<style lang="scss">
+@import './index.scss';
+
+.btn {
+  margin-top: 50rpx;
+  padding: 20rpx 40rpx;
+  background-color: #007aff;
+  color: #fff;
+  border-radius: 10rpx;
+  font-size: 32rpx;
+}
+</style>

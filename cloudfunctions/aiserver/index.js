939ee060361c4a654aa5d1f5f923dc43cb86dfd0
chenchen <929215951@qq.com>
2026-02-10 20:02:54 +0800

    fix-langchain

--- cloudfunctions/aiserver/index.js
+++ cloudfunctions/aiserver/index.js
@@ -1,9 +1,25 @@
-exports.main = async (event, context) => {
-    console.log("Received event:", JSON.stringify(event, null, 2));
-    console.log("Received context:", JSON.stringify(context, null, 2));
-    return {  
-        "hello": "world",
-        "event": event,
-        "context": context
-    }
+// 添加 ReadableStream polyfill（兼容旧版 Node.js）
+if (typeof ReadableStream === 'undefined') {
+  global.ReadableStream = require('web-streams-polyfill').ReadableStream;
 }
+
+const { ChatOpenAI } = require("@langchain/openai");
+const { HumanMessage } = require("@langchain/core/messages");
+
+exports.main = async (event, context) => {
+  const model = new ChatOpenAI({
+    baseURL: process.env.OPENAI_BASE_URL,
+    apiKey: process.env.OPENAI_API_KEY,
+    modelName: process.env.OPENAI_MODEL,
+    temperature: 0.7,
+  });
+
+  const res = await model.invoke([new HumanMessage("介绍一下你自己,你是一只胡萝卜")]);
+  console.log(res);
+  return {
+    content: res,
+    inner: '123456',
+    event,
+    context,
+  };
+};

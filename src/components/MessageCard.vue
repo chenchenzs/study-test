9dacc55a62a71a82e9b599962336d70e2d3fa1e7
chenchen <929215951@qq.com>
2026-02-11 17:52:49 +0800

    fix

---
+++ src/components/MessageCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="message-card" :class="type">
+    <image class="avatar" :src="avatar" mode="aspectFill" />
+    <view class="bubble">
+      <text class="message-text">{{ message }}</text>
+      <text v-if="time" class="message-time">{{ time }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  type?: 'user' | 'robot';
+  message: string | number;
+  avatar: string;
+  time?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  type: 'robot',
+  time: ''
+});
+</script>
+
+<style lang="scss">
+@import './MessageCard.scss';
+</style>
--- src/pages/robot/robot.vue
+++ src/pages/robot/robot.vue
@@ -1,10 +1,43 @@
 <template>
-  hello chenchen1
+  <view class="content">
+
+    <view class="messageList">
+      <scroll-view class="scroll-list" scroll-y>
+        <view v-for="(msg, index) in messageList" :key="index" :id="'msg-' + index" class="message-item">
+          <MessageCard :type="index % 2 === 0 ? 'user' : 'robot'" :message="String(msg)"
+            :avatar="index % 2 === 0 ? userAvatar : robotAvatar" :time="new Date().toLocaleTimeString()" />
+        </view>
+      </scroll-view>
+    </view>
+    <view class="inputArea">
+      <up-input placeholder="请输入内容" border="surround" v-model="userInPut" @change="handleInput">
+        <template #suffix>
+          <up-button @tap="handleSend" text="发送" type="success" size="mini"></up-button>
+        </template>
+      </up-input>
+    </view>
+  </view>
 </template>
 
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import useCloudbase from '@/utils/useCloudbase';
+import MessageCard from '@/components/MessageCard.vue';
+
+const messageList = ref(Array.from({ length: 99 }).map((v, index) => index));
+
+const userAvatar = ref('');
+const userInPut = ref('');
+const robotAvatar = ref('/static/img/avatar-robot.jpg');
+
+const handleInput = (v) => {
+  console.log(v);
+}
+
+const handleSend = (v) => {
+  console.log(v);
+}
 
 useCloudbase({
   msg: "你好"
